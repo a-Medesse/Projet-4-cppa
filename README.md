0975ffc173f4d2634ffa40de054f51d2f18d9ac5
a-Medesse <ariella.amoussou@gmail.com>
2025-01-15 11:52:08 +0100

    essai
    Co-Authored-By: AmeliaAKP <192189520+AmeliaAKP@users.noreply.github.com>
    Co-Authored-By: Arnauddeh <192413510+Arnauddeh@users.noreply.github.com>
    Co-Authored-By: misbaCPPA <192191322+misbaCPPA@users.noreply.github.com>
    Co-Authored-By: boussari-marzoukath <192526477+boussari-marzoukath@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # Projet-4-cppa
+Jeu de Morpion (Tic Tac Toe)
+
+But du Jeu:
+Le but du jeu est d'aligner avant son adversaire 3 symboles identiques horizontalement, verticalement ou en diagonale.
+
+Nombre de joueurs:
+2 joueurs
+
+Règles du jeu :
+
+->Grille de jeu :
+Le jeu se déroule sur une grille de 3x3 cases.
+Chaque case est identifiée par des coordonnées (ligne, colonne), allant de 0 à 2 pour chaque dimension.
+
+->Symbole des joueurs :
+Le joueur 1 utilise le symbole X.
+Le joueur 2 utilise le symbole O.
+
+->Tour de jeu :
+Les joueurs jouent à tour de rôle.
+À chaque tour, un joueur entre les coordonnées de la case où il souhaite placer son symbole.
+
+->Vérification du gagnant :
+Le jeu vérifie automatiquement si un joueur a aligné trois de ses symboles.
+Si oui, ce joueur est déclaré vainqueur.
+
+->Match nul :
+Si toutes les cases sont remplies sans qu'aucun joueur n'ait aligné trois symboles, la partie se termine par un match nul.
+
+->Réinitialisation :
+Après la fin d'une partie (victoire d'un joueur ou nul), le jeu propose de réinitialiser la grille pour jouer une nouvelle partie.
+
+
+
+
+
+Comment jouer:
+
+-> Démarrer le jeu :
+Lancer le programme. La grille vide sera affichée.
+
+->Entrer les coordonnées :
+Lorsqu’un joueur doit jouer, le programme demande d’entrer les coordonnées de la case sous la forme : ligne colonne (par exemple, 1 2 pour la case située à la 1e ligne et 2e colonne).
+
+->Vérification :
+Le programme vérifie si la case choisie est valide (vide et dans la grille). Si ce n'est pas le cas, il demande de rejouer.
+
+->Fin de partie :
+Quand un joueur gagne ou si la partie est nulle, le programme affiche le résultat.
+
+->Rejouer :
+Vous pouvez choisir de rejouer ou de quitter après une partie.
+
+
+
---
+++ main.c
@@ -0,0 +1,136 @@
+//
+//  main.c
+//  Jeu de morpion
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+void initialiser_grille(char grille[3][3] ){
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            grille[i][j] = ' ';
+        }
+    }
+}
+
+void afficher_grille(char grille[3][3]) {
+    printf("\n 0   1   2 <- colonnes\n");
+       for (int i = 0; i < 3; i++) {
+           printf(" %d |", i);
+           for (int j = 0; j < 3; j++) {
+               printf(" %c |", grille[i][j]);
+           }
+           printf("\n  +---+---+---+\n");
+       }
+       printf("^\n|\nlignes\n");
+}
+
+int case_vide(char grille[3][3]){
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            if (grille[i][j]==' '){
+                return 1;
+            }
+            
+            
+        }
+    }
+    return 0;
+}
+
+void choix(char grille[3][3], char symbole) {
+    int x, y;
+    char buffer[100];
+    do {
+        printf("Entrez l'abscisse(entre 0, 1 et 2):");
+        if(scanf("%d", &x) != 1) {
+            printf("Coordonnée non valide. Veuillez réessayer.\n");
+            scanf("%s", buffer);
+            continue;
+        }
+        printf("Entrez l'ordonnée(entre 0, 1 et 2):");
+        if(scanf("%d", &y) != 1) {
+            printf("Coordonnée non valide. Veuillez réessayer.\n");
+            scanf("%s", buffer);
+            continue;
+        }
+        if (x<0 || x>2 || y<0 || y>2) {
+            printf("Coordonnées non valides. Veuillez réessayer.\n");
+        }
+        else if(grille[x][y]!=' '){
+            printf("Cette case occupée, veuillez réessayer.\n");
+        }
+        else{
+            break;
+        }
+    } while (1);
+    
+    grille[x][y]=symbole;
+    }
+
+int verifier_gagnant(char grille[3][3], char symbole) {
+    for (int i = 0; i < 3; i++) {
+        if (grille[i][0] == symbole && grille[i][1] == symbole && grille[i][2] == symbole) {
+            return 1;
+        }
+    }
+
+        for (int j = 0; j < 3; j++) {
+        if (grille[0][j] == symbole && grille[1][j] == symbole && grille[2][j] == symbole) {
+            return 1;
+        }
+    }
+
+        if (grille[0][0] == symbole && grille[1][1] == symbole && grille[2][2] == symbole) {
+        return 1;
+    }
+    if (grille[0][2] == symbole && grille[1][1] == symbole && grille[2][0] == symbole) {
+        return 1;
+    }
+
+    return 0;
+}
+
+void jouer_partie(char grille[3][3]) {
+    char joueur[2] = {'A', 'B'};
+    char symbole[2] = {'X', 'O'};
+
+    initialiser_grille(grille);
+    printf("Bienvenue dans cette partie de morpion.\nJoueur A (X) \t Joueur B (O)\n");
+
+    while (1) {
+        for (int i = 0; i < 2; i++) {
+            afficher_grille(grille);
+            printf("\nC'est au tour du joueur %c (%c)\n", joueur[i], symbole[i]);
+            choix(grille, symbole[i]);
+
+            if (verifier_gagnant(grille, symbole[i])) {
+                afficher_grille(grille);
+                printf("Le joueur %c a gagné. Félicitations !\n", joueur[i]);
+                return;
+            }
+
+            if (!case_vide(grille)) {
+                afficher_grille(grille);
+                printf("Match nul !\n");
+                return;
+            }
+        }
+    }
+}
+
+
+int main(int argc, const char * argv[]) {
+    char rejouer;
+    char grille[3][3];
+
+        do {
+            jouer_partie(grille);
+
+            printf("Voulez-vous rejouer ? (o/n) : ");
+            scanf(" %c", &rejouer);
+        } while (rejouer == 'o' || rejouer == 'O');
+
+        printf("Courage ! Au revoir !\n");
+        return 0;
+}
